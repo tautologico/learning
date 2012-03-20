85f1c6a531ca418858264dbbadee625901bcf27e
Andrei Formiga <archimedes_siracusa@hotmail.com>
2012-03-20 13:04:24 -0300

    Started code for feed-forward backprop neural networks in C.

---
+++ nnets/mlp/ffbackprop.c
@@ -0,0 +1,22 @@
+/*
+ * ffbackprop.c
+ * Implementation of a feed-forward MLP neural network trained
+ * with backpropagation.
+ *
+ * Andrei de A. Formiga, 2012-03-20
+ *
+ */
+
+#include <stdio.h>
+
+// matrix of neuron weights
+unsigned char w[3][10][10];
+
+// activations
+unsigned char y[10];
+
+
+int main(int argc, char **argv)
+{
+    return 0;
+}

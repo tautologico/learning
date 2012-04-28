3a85758006e0e2bb24dfc860d65b519839a0a76c
Andrei Formiga <archimedes_siracusa@hotmail.com>
2012-04-28 02:19:35 -0300

    More markov chains.

--- mcmc/mkvchains.jl
+++ mcmc/mkvchains.jl
@@ -6,11 +6,14 @@
 # Andrei de A. Formiga, 2012-04-27
 #
 
+gothenburg_weather = [ 0.75  0.25 
+                       0.25  0.75 ]
+
 type MarkovChain
     initfn
     transmatrix
 end
 
-function simulate(MarkovChain mc)
+function simulate(mc::MarkovChain)
     println("Simulation")
 end

ecf69cf1f724969e095002f28caf8fe80e838ad6
Andrei Formiga <archimedes_siracusa@hotmail.com>
2012-04-29 01:30:16 -0300

    More tweaks in Markov chain code.

--- mcmc/mkvchains.jl
+++ mcmc/mkvchains.jl
@@ -12,16 +12,17 @@ gothenburg_weather = [ 0.75  0.25
 la_weather = [ 0.5  0.5 
                0.1  0.9 ]
 
+# constructor should verify compatibility between initvec and transmatrix
 type MarkovChain
     initvec
     transmatrix
 end
 
 # some chains based on the above matrices
-gothmc1 = MarkovChain([1.0, 0.0], gothenburg_weather)  # start always at first state (sunny)
-gothmc2 = MarkovChain([0.5, 0.5], gothenburg_weather) 
+gothmc1 = MarkovChain([1.0  0.0], gothenburg_weather)  # start always at first state (sunny)
+gothmc2 = MarkovChain([0.5  0.5], gothenburg_weather) 
 
-lamc1 = MarkovChain([0.5, 0.5], la_weather)
+lamc1 = MarkovChain([0.5  0.5], la_weather)
 
 # return the cumulative sum of a, because cumsum() sometimes
 # returns a tuple
@@ -56,17 +57,19 @@ end
 # return the number of steps with state = 1 for each step
 function simulate(mc::MarkovChain, steps::Int)
     res = zeros(Int, steps)
+    dist = mc.initvec
     state = init(mc)
     res[1] = state == 1 ? 1 : 0
     for i = 2:steps
         state = simulation_step(mc, state)
+        dist *= mc.transmatrix
         res[i] = res[i-1] + (state == 1 ? 1 : 0)
     end
-    res
+    (res, dist)
 end
 
 # calculate the proportion of steps with state equal to 1 for each step, 
 # given the cumulative number of steps with state = 1
-s1proportion(states) = states ./ [1:length(states)]
+state1_proportion(states) = states ./ [1:length(states)]
 
     

064019f73741b04a7097322cdb9f232074c28a58
Andrei Formiga <archimedes_siracusa@hotmail.com>
2012-04-30 00:29:33 -0300

    Implemented MCMC simulation for the hardcore model.

--- mcmc/mkvchains.jl
+++ mcmc/mkvchains.jl
@@ -73,3 +73,78 @@ end
 state1_proportion(states) = states ./ [1:length(states)]
 
     
+# create a grid for the hardcore model with n lines, m columns
+create_hardcore_grid(n, m) = zeros(Uint8, n, m)
+
+function print_grid(grid)
+    for i = 1:size(grid, 1)-1
+        for j = 1:size(grid, 2)-1
+            print(grid[i, j] == 0 ? "O" : "X")
+            print(" -- ")
+        end
+        println(grid[i, size(grid, 2)] == 0 ? "O" : "X")
+        for j = 1:size(grid, 2)
+            print("|    ")
+        end
+        println("")
+    end
+    for j = 1:size(grid, 2)-1
+        print(grid[size(grid, 1), j] == 0 ? "O" : "X")
+        print(" -- ")
+    end
+    println(grid[size(grid, 1), size(grid, 2)] == 0 ? "O" : "X")
+end
+    
+function copy_neighbors(grid, i, j, neighbors)
+    ix = 1
+    if i-1 >= 1
+        neighbors[ix] = grid[i-1, j]
+        ix += 1
+    end
+    if j-1 >= 1
+        neighbors[ix] = grid[i, j-1]
+        ix += 1
+    end
+    if j+1 <= size(grid, 2)
+        neighbors[ix] = grid[i, j+1]
+        ix += 1
+    end
+    if i+1 <= size(grid, 1)
+        neighbors[ix] = grid[i+1, j]
+        ix += 1
+    end
+end
+
+# returns a vector with the values of all neighbors of 
+# the position at line i, column j
+function neighbors(grid, i, j)
+    w = (i > 1 && i < size(grid, 1) ? 2 : 1)
+    h = (j > 1 && j < size(grid, 2) ? 2 : 1)
+    res = zeros(eltype(grid), w + h)
+    copy_neighbors(grid, i, j, res)
+    res
+end
+
+function hardcore_mcmc(grid, steps)
+    k = length(grid)
+    delta = 1.0 / (2 * k)
+    choices = [0.0:delta:1.0]
+    for s = 1:steps
+        r = rand()
+        i = 1
+        while choices[i] <= r && i < 2 * k
+            i += 1
+        end
+        # check if heads or tails
+        heads = (i % 2 == 0)
+        v = div(i-1, 2) + 1
+        ig = mod(v-1, size(grid, 1)) + 1
+        jg = div(v-1, size(grid, 1)) + 1
+        #println("i = $i, v = $v, ig = $ig, jg = $jg")
+        if heads && max(neighbors(grid, ig, jg)) == 0
+            grid[ig, jg] = 1
+        else
+            grid[ig, jg] = 0
+        end
+    end
+end

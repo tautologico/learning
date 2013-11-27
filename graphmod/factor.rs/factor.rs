90e05996c8e1f674b7f03e1652c78304ac7f2364
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-11-27 16:08:25 -0300

    More types

--- graphmod/factor.rs/factor.rs
+++ graphmod/factor.rs/factor.rs
@@ -6,11 +6,13 @@ trait ValueMapping {
 
 type ValMap<'self> = 'self |uint| -> ~str;
 
+#[deriving(Eq)]
 struct Type {
 	name: ~str,
 	nvalues: uint
 }
 
+#[deriving(Eq)]
 struct Var {
 	name: ~str,
 	typ: Type
@@ -21,14 +23,34 @@ struct Value {
 	val: uint
 }
 
+type Inst = ~[(Var, Value)];
+
 struct Instantiation {
 	vars: ~[Var],
 	values: ~[Value]
 }
 
 struct Factor {
-	nvars: int
+	normalized: bool,
+	vars: ~[Var],
+	values: ~[Value]
 }
+
+// --- operations ---------------------------------------------------
+
+/// Returns variables in factor f that are not in vars
+fn sum_factor_vars(f: &Factor, vars: &[Var]) -> ~[Var] {
+	f.vars.iter().filter(|x| !vars.contains(*x)).collect()
+}
+
+// fn sum_out_vars(f: &Factor, vars: &[Var]) -> Factor {
+// 	let res_vars = sum_factor_vars(f, vars);
+// 	// TODO
+// }
+
+// fn multiply_factors(f1: &Factor, f2: &Factor) -> Factor {
+// 	// TODO 
+// }
 
 #[test]
 fn trivial_test() {

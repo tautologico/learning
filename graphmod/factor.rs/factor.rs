72b6fed64c018f9bf82008c9545b9dcd9fdbc7bb
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-11-27 02:15:22 -0300

    A few types

--- graphmod/factor.rs/factor.rs
+++ graphmod/factor.rs/factor.rs
@@ -1,4 +1,37 @@
 
+trait ValueMapping {
+	fn val_to_str(&self, val: uint) -> ~str;
+	fn str_to_val(&self, &str) -> uint;
+}
+
+type ValMap<'self> = 'self |uint| -> ~str;
+
+struct Type {
+	name: ~str,
+	nvalues: uint
+}
+
+struct Var {
+	name: ~str,
+	typ: Type
+}
+
+struct Value {
+	typ: Type,
+	val: uint
+}
+
+struct Instantiation {
+	vars: ~[Var],
+	values: ~[Value]
+}
+
 struct Factor {
-	int nvars;
+	nvars: int
+}
+
+#[test]
+fn trivial_test() {
+	let x = 2;
+	println!("This is a test {}", x)
 }

ef22fbed5a0af8f8bd283bd52e92da2d02e320d3
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-11-20 00:35:15 -0300

    dictionary reading test

--- linprog/test/test.ml
+++ linprog/test/test.ml
@@ -1,2 +1,17 @@
 open OUnit2
 
+let test_read_dict ctxt = 
+  let f = open_in "test/part1/dict1" in
+  let d = Dict.read f in
+  assert_equal d.m 3;
+  assert_equal d.n 4;
+  assert_equal (Array.length d.basis) d.m;
+  assert_equal d.basis.(0) 1
+
+
+let suite = 
+  "Unit tests" >:::
+    [ "dictionary reading" >:: test_read_dict ]
+
+let () = 
+  run_test_tt_main suite
